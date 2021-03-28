a3cd38e0c7fd223067e8d78efc67cdd8715ae26a
Timur <tashurbekov@gmail.com>
2021-03-28 16:07:51 +0300

    Практика: Сравнение строк

---
+++ week2/lesson_if_2.py
@@ -0,0 +1,26 @@
+# Практика: Сравнение строк
+# Написать функцию, которая принимает на вход две строки
+# Проверить, является ли то, что передано функции, строками. Если нет - вернуть 0
+# Если строки одинаковые, вернуть 1
+# Если строки разные и первая длиннее, вернуть 2
+# Если строки разные и вторая строка 'learn', возвращает 3
+# Вызвать функцию несколько раз, передавая ей разные параметры и выводя на экран результаты
+
+
+def str_comp(str1, str2):
+    if not isinstance(str1, str) or not isinstance(str2, str):
+        return 0
+    elif str1 == str2:
+        return 1
+    elif len(str1) > len(str2):
+        return 2
+    elif str1 != str2 and str2 == 'learn':
+        return 3
+    else:
+        return 'Ни одно условие не выполнено'
+
+print(str_comp(5, 'test'))
+print(str_comp('test', 'test'))
+print(str_comp('teeeest', 'test'))
+print(str_comp('test', 'learn'))
+
